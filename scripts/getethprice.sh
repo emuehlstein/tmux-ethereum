07f3f52b5408c990c95766319e013f86bdd52a92
Eric Muehlstein <muehlbucks@gmail.com>
2018-07-27 10:47:25 -0400

    fix json parsing

--- scripts/getethprice.sh
+++ scripts/getethprice.sh
@@ -6,5 +6,5 @@ import requests
 
 cryptorequest = requests.get('https://api.coinmarketcap.com/v2/ticker/1027/')
 currencyjson = json.loads(cryptorequest.text)
-wholedollarvalue = int(round(float(currencyjson[0]["price_usd"])))
+wholedollarvalue = int(round(float(currencyjson['data']['quotes']['USD']['price'])))
 print(wholedollarvalue)
