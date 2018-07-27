3538bc9aa00aa26499af0656f624da272de30aeb
Eric Muehlstein <muehlbucks@gmail.com>
2018-07-26 21:56:20 -0400

    changing to v2 API

--- scripts/getethprice.sh
+++ scripts/getethprice.sh
@@ -4,7 +4,7 @@ import sys
 import json
 import requests
 
-cryptorequest = requests.get('https://api.coinmarketcap.com/v1/ticker/ethereum/')
+cryptorequest = requests.get('https://api.coinmarketcap.com/v2/ticker/1027/')
 currencyjson = json.loads(cryptorequest.text)
 wholedollarvalue = int(round(float(currencyjson[0]["price_usd"])))
 print(wholedollarvalue)
